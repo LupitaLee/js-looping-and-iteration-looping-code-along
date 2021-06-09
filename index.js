453f4a0183c7cd958abcdc5d36dd2988b10132ad
Lupita <lupitarivera8899@gmail.com>
2021-06-09 13:30:00 -0500

    commit tree 9f49b72541ec94da51f2e3cf4a2e14c16376c48e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const array = ['Lisa', 'Laura', 'lupita']
+
+const string = 'birday'
+
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+
+
+function countDown(num){
+    
+    while (num >= 0){
+        console.log(num--)
+    }
+}
